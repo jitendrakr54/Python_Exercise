f1f9a35a89ba132990f3189ca8406edeea417747
Jitendra Kumar Gupta <jitendrakr54@gmail.com>
2018-07-18 22:10:43 +0530

    divisor-check

---
+++ Basics/Python002.py
@@ -0,0 +1,14 @@
+# Ask the user for two numbers: one number to check (call it num) and one number to divide by (check).
+# If check divides evenly into num, tell that to the user. If not, print a different appropriate message.
+
+def main():
+    dividend = int(input("Enter the dividend"))
+    divisor = int(input("Enter the divisor now"))
+
+    if dividend % divisor == 0:
+        print(str(dividend) +" is divisible by "+ str(divisor))
+    else:
+        print("not divisible")
+
+if __name__ == "__main__":
+    main()
