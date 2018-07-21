f9d289b43cea6d61d494c3c427a9efd11c69a4f6
Jitendra Kumar Gupta <jitendrakr54@gmail.com>
2018-07-21 22:26:22 +0530

    moduling-codes

---
+++ Basics/main.py
@@ -0,0 +1,11 @@
+from Python001 import *
+from Python002 import *
+from Python003 import *
+
+def main():
+    # checkAge()
+    # checkIfDivisible()
+    printDivisibleByInputedNo()
+
+if __name__ == "__main__":
+    main()
