f3a455df45e64dad128c985604cda54dfbbe368a
Jitendra Kumar Gupta <jitendrakr54@gmail.com>
2018-07-29 11:34:25 +0530

    greatest-of-three-no

--- Basics/main.py
+++ Basics/main.py
@@ -1,11 +1,13 @@
 from Python001 import *
 from Python002 import *
 from Python003 import *
+from Python004 import *
 
 def main():
     # checkAge()
     # checkIfDivisible()
-    printDivisibleByInputedNo()
+    # printDivisibleByInputedNo()
+    greatestFromThreeNo()
 
 if __name__ == "__main__":
     main()
